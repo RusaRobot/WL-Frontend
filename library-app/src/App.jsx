836f551b122440b9a2ad99d127c74871dc0bee79
RizkyMilan <mhmrizky33@gmail.com>
2022-10-14 15:35:29 +0700

    Homepage done R

--- library-app/src/App.jsx
+++ library-app/src/App.jsx
@@ -1,5 +1,5 @@
 import {
-    Image,
+  Image,
   Box,
   Spacer,
   Flex,
@@ -7,124 +7,120 @@ import {
   InputGroup,
   Button,
   Input,
-} from "@chakra-ui/react"
-import { Link, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import LoginPage from "./pages/LoginPage"
-import RegisterPage from "./pages/RegisterPage"
-import Book from "./pages/Book"
-
-
+} from "@chakra-ui/react";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Book from "./pages/Book";
 
 const App = () => {
-    return (
-        // {Navbar}
-        <>
-            <Box bgColor={"#9E7676"} pt={"0,5"} pb={"0,5"}>
-                <Flex display={"flex"}>
-                    <Image src="https://i.ibb.co/fXPc0Mx/Krusty-Krab-free-file-1.png" alt="Krusty Krab" border="0" height={"50px"} />                    
-                    <Link to="/">
-                        <Box
-                            p="4"
-                            color="white"
-                            _hover={{
-                                background: "white",
-                                color: "black",
-                                transition: "all 10 00ms ease",
-                                cursor: "pointer",
-                            }}
-                        >
-                            Home
-                        </Box>
-                    </Link>
-                    <Box
-                        p="4"
-                        color="white"
-                        _hover={{
-                            background: "white",
-                            color: "black",
-                            transition: "all 1000ms ease",
-                            cursor: "pointer",
-                        }}
-                    >
-                        About
-                    </Box>
-                    <Box
-                        p="4"
-                        color="white"
-                        _hover={{
-                            background: "white",
-                            color: "black",
-                            transition: "all 1000ms ease",
-                            cursor: "pointer",
-                        }}
-                    >
-                        Catalog Book
-                    </Box>
-                    <Box
-                        p="4"
-                        color="white"
-                        _hover={{
-                            background: "white",
-                            color: "black",
-                            transition: "all 1000ms ease",
-                            cursor: "pointer",
-                        }}
-                    >
-                        Help
-                    </Box>
-                    <Spacer />
-                    <Box
-                        p="4"
-                        color="white"
-                        _hover={{
-                            background: "white",
-                            color: "black",
-                            transition: "all 1000ms ease",
-                            cursor: "pointer",
-                        }}
-                    >
-                        <Link to="/login">Login</Link>
-                    </Box>
-                    <Box
-                        p="4"
-                        color="white"
-                        _hover={{
-                            background: "white",
-                            color: "black",
-                            transition: "all 1000ms ease",
-                            cursor: "pointer",
-                        }}
-                    >
-                        <Link to="/register">Register</Link>
-                    </Box>
-                    <Box mr="2" mt="2" mb="2">
-                        <InputGroup>
-                            <Input
-                                placeholder="search here"
-                                color={"white"}
-                            ></Input>
-                            <InputRightElement width={"2,5 rem"}>
-                                <Button
-                                    _hover={{ background: "white" }}
-                                    color={"white.300"}
-                                >
-                                    Search
-                                </Button>
-                            </InputRightElement>
-                        </InputGroup>
-                    </Box>
-                </Flex>
+  return (
+    // {Navbar}
+    <>
+      <Box bgColor={"#9E7676"} pt={"0,5"} pb={"0,5"}>
+        <Flex display={"flex"}>
+          <Image
+            src="https://i.ibb.co/fXPc0Mx/Krusty-Krab-free-file-1.png"
+            alt="Krusty Krab"
+            border="0"
+            height={"50px"}
+          />
+          <Link to="/">
+            <Box
+              p="4"
+              color="white"
+              _hover={{
+                background: "white",
+                color: "black",
+                transition: "all 10 00ms ease",
+                cursor: "pointer",
+              }}
+            >
+              Home
             </Box>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/login" element={<LoginPage />} />
-            </Routes>
-        </>
-        //   {/* navbar */}
-    )
+          </Link>
+          <Box
+            p="4"
+            color="white"
+            _hover={{
+              background: "white",
+              color: "black",
+              transition: "all 1000ms ease",
+              cursor: "pointer",
+            }}
+          >
+            About
+          </Box>
+          <Box
+            p="4"
+            color="white"
+            _hover={{
+              background: "white",
+              color: "black",
+              transition: "all 1000ms ease",
+              cursor: "pointer",
+            }}
+          >
+            Catalog Book
+          </Box>
+          <Box
+            p="4"
+            color="white"
+            _hover={{
+              background: "white",
+              color: "black",
+              transition: "all 1000ms ease",
+              cursor: "pointer",
+            }}
+          >
+            Help
+          </Box>
+          <Spacer />
+          <Box
+            p="4"
+            color="white"
+            _hover={{
+              background: "white",
+              color: "black",
+              transition: "all 1000ms ease",
+              cursor: "pointer",
+            }}
+          >
+            <Link to="/login">Login</Link>
+          </Box>
+          <Box
+            p="4"
+            color="white"
+            _hover={{
+              background: "white",
+              color: "black",
+              transition: "all 1000ms ease",
+              cursor: "pointer",
+            }}
+          >
+            <Link to="/register">Register</Link>
+          </Box>
+          <Box mr="2" mt="2" mb="2">
+            <InputGroup>
+              <Input placeholder="search here" color={"white"}></Input>
+              <InputRightElement width={"2,5 rem"}>
+                <Button _hover={{ background: "white" }} color={"white.300"}>
+                  Search
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </Box>
+        </Flex>
+      </Box>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </>
+    //   {/* navbar */}
+  );
+};
 
-}
-
-export default App
+export default App;
