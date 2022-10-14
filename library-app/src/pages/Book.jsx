0e5248b7d6b60c237bf858e8c7511cbfd39e8602
dwitap <dwitap.1223@gmail.com>
2022-10-14 15:18:07 +0700

    detail book page

--- library-app/src/pages/Book.jsx
+++ library-app/src/pages/Book.jsx
@@ -4,67 +4,66 @@ import { axiosInstance } from "../api"
 import BookCollection from "../components/bookCollection"
 
 const Book = () => {
-  const [book, setBook] = useState([])
-  const [totalCount, setTotalCount] = useState(0)
-  const [page, setPage] = useState(1)
+    const [book, setBook] = useState([])
+    const [totalCount, setTotalCount] = useState(0)
+    const [page, setPage] = useState(1)
 
-  const fetchBooks = async () => {
-    try {
-      const response = await axiosInstance.get("/book", {
-        params: {
-          _order: "DESC",
-          _limit: 2,
-          _page: page,
-        },
-      })
+    const fetchBooks = async () => {
+        try {
+            const response = await axiosInstance.get("/book", {
+                params: {
+                    _order: "DESC",
+                    _limit: 2,
+                    _page: page,
+                },
+            })
 
-    //   setTotalCount(response.data.data)
+            //   setTotalCount(response.data.data)
 
-    setBook(response.data.data)
-    // if (page === 1) {
-    //   } else {
-    //     setPosts([...posts, ...response.data.data])
-    //   }
-    } catch (err) {
-      console.log(err)
+            setBook(response.data.data)
+            // if (page === 1) {
+            //   } else {
+            //     setPosts([...posts, ...response.data.data])
+            //   }
+        } catch (err) {
+            console.log(err)
+        }
     }
-  }
 
-  useEffect(() => {
-    fetchBooks()
-  }, [])
+    useEffect(() => {
+        fetchBooks()
+    }, [])
 
-  const renderBooks = () => {
-    return book.map((val) => {
-      return (
-        <BookCollection
-          key={val.id.toString()}
-          title={val.title}
-          author={val.author}
-          release_year={val.release_year}
-          genre={val.genre}
-          language={val.language}
-        />
-      )
-    })
-  }
-//   title, author, release_year, genre, language
+    const renderBooks = () => {
+        return book.map((val) => {
+            return (
+                <BookCollection
+                    key={val.id.toString()}
+                    title={val.title}
+                    author={val.author}
+                    release_year={val.release_year}
+                    genre={val.genre}
+                    language={val.language}
+                    id={val.id}
+                />
+            )
+        })
+    }
+    //   title, author, release_year, genre, language
 
-  
-
-  return (
-    <>
-      <Box fontWeight={"bold"}>Books</Box>
-      <HStack>
-        <Box>title</Box>
-        <Box>author</Box>
-        <Box>release year</Box>
-        <Box>genre</Box>
-        <Box>language</Box>
-      </HStack>
-      {renderBooks()}
-    </>
-  )
+    return (
+        <>
+            <Box fontWeight={"bold"}>Books</Box>
+            <HStack>
+                <Box>title</Box>
+                <Box>author</Box>
+                <Box>release year</Box>
+                <Box>genre</Box>
+                <Box>language</Box>
+            </HStack>
+            {renderBooks()}
+        </>
+    )
 }
 
 export default Book
